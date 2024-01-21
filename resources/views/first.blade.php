368fbd14be2da433cca16cedc0bdaffd5d7e3bec
Jose Pernia <jose.pernia@bipay.cl>
2024-01-21 03:52:29 +0000

    change

--- resources/views/first.blade.php
+++ resources/views/first.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Dividir pantalla en dos partes iguales con Bootstrap</title>
+  <title>Sibus - System</title>
   <!-- Enlace al archivo CSS de Bootstrap -->
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
